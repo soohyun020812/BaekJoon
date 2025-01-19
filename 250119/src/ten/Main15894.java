adcc2b7c4374d1c5bd5204f7dcb9ae3d58ad8b58
SooHyun Lee <131852352+soohyun020812@users.noreply.github.com>
2025-01-19 17:19:21 +0900

    Update Main15894.java

--- 250119/src/ten/Main15894.java
+++ 250119/src/ten/Main15894.java
@@ -8,13 +8,11 @@ public class Main15894 {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        // 입력 받기
         long n = Long.parseLong(br.readLine());
         
         // 둘레 계산
         long perimeter = 4 * n;
         
-        // 결과 출력
         System.out.println(perimeter);
     }
 }
