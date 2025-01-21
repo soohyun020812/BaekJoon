0f419a43b3f27cfd878cbe26a5ee5b62d15b464e
SooHyun Lee <131852352+soohyun020812@users.noreply.github.com>
2025-01-21 23:09:11 +0900

    Add files via upload

---
+++ 250121/src/_11/Main24262.java
@@ -0,0 +1,17 @@
+package _11;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+// 24262 알고리즘 수업 - 알고리즘의 수행 시간 1
+public class Main24262 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        // 코드1의 수행 횟수
+        System.out.println(1); 
+        // 최고차항 차수
+        System.out.println(0); 
+    }
+}
---
+++ 250121/src/_11/Main24263.java
@@ -0,0 +1,17 @@
+package _11;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+// 24263 알고리즘 수업 - 알고리즘 수행 시간 2
+public class Main24263 {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        // 코드1의 수행 횟수
+        System.out.println(n); 
+        // 최고차항 차수
+        System.out.println(1); 
+    }
+}
