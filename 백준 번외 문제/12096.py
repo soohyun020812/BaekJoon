f38cc37af6db3949f8e894309f5a560f6405a937
SooHyun Lee <131852352+soohyun020812@users.noreply.github.com>
2025-02-03 17:18:58 +0900

    Add files via upload

---
+++ 백준 번외 문제/12096.py
@@ -0,0 +1,36 @@
+# 
+
+'''
+문제
+
+입력
+
+출력
+'''
+
+# 문제 페이지 소스코드에 Base64로 Encoding된 주석들이 등장하는데
+# 이 주석들을 Decoding하면 힌트를 확인할 수 있다
+
+'''
+# 주석 처리 제거
+{
+    "12096": {
+        "id": "12096",
+        "problem_description": "<!-- 66y47KCc7J2YIOygleuLteydgA== -->",
+        "problem_input": "<!-- 7JWM66Ck7KSEIOyImCDsl4bri6Q= -->",
+        "problem_output": "<!-- 7ZWY7KeA66eMIO2ejO2KuOuKlCDsnojri6Q= -->",
+        "save_date": 1735393890514
+    },
+    "last_check_date": 1735393599607
+}
+
+# Base64 Decoding
+문제의 정답은
+알려줄 수 없다
+하지만 힌트는 있다
+힌트는 https://startlink.io/ 에 있는 ㅈㅎㅂㅎ
+'''
+
+print("02-521-0487")
+
+# 문제를 일반적으로 보지 않고 개발자 도구를 활용하는 게 인상 깊었다 
